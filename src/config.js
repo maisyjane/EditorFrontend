8545d8d26a91f3eb77bb9a868ddb052d640a6607
40225143 <msinclair06@qub.ac.uk>
2021-12-10 20:46:58 +0000

    added async and started config file

---
+++ src/config.js
@@ -0,0 +1,8 @@
+let wordcountURL = "http://wordcount.editor.qpc.hal.davecutting.uk";
+let charcountURL = "http://charcount.editor.qpc.hal.davecutting.uk";
+let vowelcountURL = "http://vowelcount.40225143.qpc.hal.davecutting.uk";
+let instancesOfAndURL = "http://instancesofand.40225143.qpc.hal.davecutting.uk";
+let commaCountURL = "http://commacount.40225143.qpc.hal.davecutting.uk";
+let averageWordLengthURL = "http://averagewordlength.40225143.qpc.hal.davecutting.uk";
+let proxyUrl = "http://proxy.40225143.qpc.hal.davecutting.uk/";
+let proxylinks = ["http://proxy.40225143.qpc.hal.davecutting.uk/", "http://deadproxy.40225143.qpc.hal.davecutting.uk/", "http://proxy2.40225143.qpc.hal.davecutting.uk/", "http://proxy3.40225143.qpc.hal.davecutting.uk/"];

483ec33dbe1b1bc4d3ed591063df55f07540d17b
40225143 <msinclair06@qub.ac.uk>
2021-12-13 12:32:41 +0000

    stateful saving

--- src/config.js
+++ src/config.js
@@ -1,8 +1,13 @@
-let wordcountURL = "http://wordcount.editor.qpc.hal.davecutting.uk";
-let charcountURL = "http://charcount.editor.qpc.hal.davecutting.uk";
-let vowelcountURL = "http://vowelcount.40225143.qpc.hal.davecutting.uk";
-let instancesOfAndURL = "http://instancesofand.40225143.qpc.hal.davecutting.uk";
-let commaCountURL = "http://commacount.40225143.qpc.hal.davecutting.uk";
-let averageWordLengthURL = "http://averagewordlength.40225143.qpc.hal.davecutting.uk";
-let proxyUrl = "http://proxy.40225143.qpc.hal.davecutting.uk/";
-let proxylinks = ["http://proxy.40225143.qpc.hal.davecutting.uk/", "http://deadproxy.40225143.qpc.hal.davecutting.uk/", "http://proxy2.40225143.qpc.hal.davecutting.uk/", "http://proxy3.40225143.qpc.hal.davecutting.uk/"];
+var urls = {
+wordcountURL : "http://wordcount.40225143.qpc.hal.davecutting.uk/",
+charcountURL : "http://charcount.editor.qpc.hal.davecutting.uk",
+vowelcountURL : "http://vowelcount.40225143.qpc.hal.davecutting.uk",
+instancesOfAndURL : "http://instancesofand.40225143.qpc.hal.davecutting.uk",
+commaCountURL : "http://commacount.40225143.qpc.hal.davecutting.uk",
+averageWordLengthURL : "http://averagewordlength.40225143.qpc.hal.davecutting.uk",
+proxyUrl : "http://proxy.40225143.qpc.hal.davecutting.uk/",
+proxylink1 : "http://proxy.40225143.qpc.hal.davecutting.uk/",
+proxylink2 :  "http://deadproxy.40225143.qpc.hal.davecutting.uk/",
+proxylink3: "http://proxy2.40225143.qpc.hal.davecutting.uk/",
+proxylink4: "http://proxy3.40225143.qpc.hal.davecutting.uk/"
+}
